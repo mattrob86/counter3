6246eab8c16988358241448c1534205ebad454a4
unknown <mattrob86@aol.com>
2020-02-05 21:47:16 -0500

    Added css and buttons to js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React {Component} from 'react';
+import React, {Component} from 'react';
+import "./App.css"
 
-class helloWorld extends Component {
+class Counter extends Component {
   constructor () {
     super();
     this.state = {
-count: 0,
+      count: 0,
     }
   }
+// this is where methods live
+
+increment = () => {
+  this.setState ({
+    count: this.state.count + 1,
+  })
+}
+
+decrement = () => {
+  this.setState ({
+    count: this.state.count - 1,
+  })
+}
 
 render() {
-  return {
-    <div>
-
+  return (
+    <div className="container">
+    <div className="navbar">Counter</div>
+    <div className="counter">
+      <h1>{this.state.count}</h1>
+      <button type="button" onClick={this.increment}>Increment</button>
+      <button type="button" onClick={this.decrement}>Decrement</button>
     </div>
-  }
+    </div>
+  )
  }
-}
+};
 
 export default Counter;
