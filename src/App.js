df5e8fe90f7bb35c0a24188d744e5af4c5d5e0da
unknown <mattrob86@aol.com>
2020-02-19 20:00:56 -0500

    Added changes to the app - double and clear button

--- src/App.js
+++ src/App.js
@@ -1,39 +1,77 @@
-import React, {Component} from 'react';
-import "./App.css"
+import React, { Component } from 'react';
 
 class Counter extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       count: 0,
+      doubleCount: false,
     }
   }
-// this is where methods live
 
-increment = () => {
-  this.setState ({
-    count: this.state.count + 1,
-  })
+  //this is where we are going to put our methods yay! :)
+  increment = () => {
+    if (this.state.count < 20) {
+      this.setState({
+        count: this.state.count + 1,
+      })
+    }
+    if (this.state.doubleCount) {
+      if (this.state.count < 19) {
+        this.setState({
+          count: this.state.count + 2,
+        })
+      }
+    }
+  }
+
+  decrement = () => {
+    if (this.state.count > 0) {
+      this.setState({
+        count: this.state.count - 1,
+      })
+    }
+    if (this.state.doubleCount) {
+      if (this.state.count > 1) {
+        this.setState({
+          count: this.state.count - 2,
+        })
+      }
+    }
+  }
+
+  clear = () => {
+    this.setState({
+      count: 0,
+    })
+  }
+
+  double = () => {
+    if (this.state.doubleCount) {
+      this.setState({
+        doubleCount: false,
+      })
+    } else {
+      this.setState({
+        doubleCount: true,
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="navbar">Counter</div>
+        <div className="counter">
+          <h1>{this.state.count}</h1>
+          <button type="button" onClick={this.clear}>Clear</button>
+          <button type="button" onClick={this.increment}>Increment</button>
+          <button type="button" onClick={this.decrement}>Decrement</button>
+          <button type="button" onClick={this.double}>{this.state.doubleCount ? 'Double Count' : 'Single Count'}</button>
+        </div>
+      </div>
+    )
+  }
 }
-
-decrement = () => {
-  this.setState ({
-    count: this.state.count - 1,
-  })
-}
-
-render() {
-  return (
-    <div className="container">
-    <div className="navbar">Counter</div>
-    <div className="counter">
-      <h1>{this.state.count}</h1>
-      <button type="button" onClick={this.increment}>Increment</button>
-      <button type="button" onClick={this.decrement}>Decrement</button>
-    </div>
-    </div>
-  )
- }
-};
 
 export default Counter;
